f130f3540fe5193cd745dc3ae943649a958e6980
Austin Frick <austin.frick429@gmail.com>
2024-07-18 21:31:13 -0400

    Viewmodels filled out

---
+++ Models/ViewModels/Customer/CustomerIndexModel.cs
@@ -0,0 +1,8 @@
+namespace GeneralStoreMVC.Models.Customer;
+
+public class CustomerIndexViewModel
+{
+    public int id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
