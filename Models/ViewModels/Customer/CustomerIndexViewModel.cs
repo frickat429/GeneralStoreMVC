d42502c931beaacdcdf8caed0f7b988077a4c0df
Austin Frick <austin.frick429@gmail.com>
2024-07-23 19:27:51 -0400

    Customer index complete/ code cleanup

--- Models/ViewModels/Customer/CustomerIndexViewModel.cs
+++ Models/ViewModels/Customer/CustomerIndexViewModel.cs
@@ -2,7 +2,7 @@ namespace GeneralStoreMVC.Models.Customer;
 
 public class CustomerIndexViewModel
 {
-    public int id { get; set; }
+    public int Id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
 }
